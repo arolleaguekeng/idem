154c9d3e95b926c2a1c104cc55819d0109fe2228
arolleaguekeng <arolleaguekeng@gmail.com>
2025-03-29 20:29:19 +0100

    create environement files if not exist

--- mynode.js
+++ mynode.js
@@ -3,7 +3,8 @@ const path = require('path');
 const successColor = '\x1b[32m%s\x1b[0m';
 const checkSign = '\u{2705}';
 require('dotenv').config({ path: 'src/.env' });;
-
+const fs = require('fs');
+const path = require('path');
 const envFile = `export const environment = {
   environment: 'prod',
   firebase: {
@@ -17,23 +18,21 @@ const envFile = `export const environment = {
 `;
 
 
+
+
+
 const targetPath = path.join(__dirname, './src/environments/environment.ts');
-fs.writeFile(targetPath, envFile, (err) => {
-    if (err) {
-        console.error(err);
-        throw err;
-    } else {
-        console.log(successColor, `${checkSign} Successfully generated environment.ts`);
-    }
-});
-
-
 const targetDevPath = path.join(__dirname, './src/environments/environment.development.ts');
-fs.writeFile(targetDevPath, envFile, (err) => {
-    if (err) {
-        console.error(err);
-        throw err;
+
+function createFileIfNotExists(filePath, content) {
+    if (!fs.existsSync(filePath)) {
+        fs.writeFileSync(filePath, content);
+        console.log(`✅ Successfully created: ${filePath}`);
     } else {
-        console.log(successColor, `${checkSign} Successfully generated environment.development.ts`);
+        console.log(`⚠️ File already exists: ${filePath}`);
     }
-});
+}
+
+// Création des fichiers seulement s'ils n'existent pas
+createFileIfNotExists(targetPath, envFile);
+createFileIfNotExists(targetDevPath, envFile);
