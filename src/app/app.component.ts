dbdea9c4b2051d7ac58852c6abb558a79c877dd6
arolleaguekeng <arolleaguekeng@gmail.com>
2025-03-31 02:11:18 +0100

    change project name "Lexis" to "Lexi"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./modules/landing/components/header/header.component";
-import { FooterComponent } from "./modules/landing/components/footer/footer.component";
+import { HeaderComponent } from './modules/landing/components/header/header.component';
+import { FooterComponent } from './modules/landing/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'lexis';
+  title = 'lexi';
 }
