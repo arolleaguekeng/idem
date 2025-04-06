8d07d507342fef9f326397b9919b282c5b8199b6
arolleaguekeng <arolleaguekeng@gmail.com>
2025-04-06 14:08:42 +0100

    migrate to nx

---
+++ lexi/apps/lexi-shell/src/app/app.config.ts
@@ -0,0 +1,103 @@
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+  REQUEST,
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+import { routes } from './app.routes';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import {
+  getAnalytics,
+  provideAnalytics,
+  ScreenTrackingService,
+  UserTrackingService,
+} from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+
+import { environment } from '../../../../environments/environment';
+import { provideHttpClient } from '@angular/common/http';
+import { MERMAID_OPTIONS, provideMarkdown } from 'ngx-markdown';
+import { AuthService } from './modules/auth/services/auth.service';
+import { FIREBASE_OPTIONS, FirebaseApp } from '@angular/fire/compat';
+
+import { PLATFORM_ID, inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { initializeServerApp } from '@angular/fire/app';
+import { ProjectService } from './modules/dashboard/services/project.service';
+
+const firebaseConfig = {
+  apiKey: environment.firebase.apiKey,
+  authDomain: environment.firebase.authDomain,
+  projectId: environment.firebase.projectId,
+  storageBucket: environment.firebase.storageBucket,
+  messagingSenderId: environment.firebase.messagingSenderId,
+  appId: environment.firebase.appId,
+};
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+
+    provideFirebaseApp(() => {
+      if (isPlatformBrowser(inject(PLATFORM_ID))) {
+        return initializeApp(firebaseConfig);
+      }
+      // Optional, since it's null in dev-mode and SSG
+      const request = inject(REQUEST, { optional: true });
+      const authHeader = request?.headers?.get('authorization');
+
+      const authIdToken = authHeader?.startsWith('Bearer ')
+        ? authHeader.split('Bearer ')[1]
+        : undefined;
+      return initializeServerApp(firebaseConfig, {
+        authIdToken,
+        releaseOnDeref: request || undefined,
+      });
+    }),
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics()),
+    ScreenTrackingService,
+    UserTrackingService,
+    provideFirestore(() => getFirestore()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideStorage(() => getStorage()),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          prefix: 'p',
+          darkModeSelector: 'system',
+          cssLayer: false,
+        },
+      },
+    }),
+    provideAnimations(),
+    provideHttpClient(),
+    provideMarkdown({
+      mermaidOptions: {
+        provide: MERMAID_OPTIONS,
+        useValue: {
+          darkMode: true,
+          look: 'handDrawn',
+        },
+      },
+    }),
+    AuthService,
+    ProjectService,
+    { provide: FIREBASE_OPTIONS, useValue: environment.firebase },
+  ],
+};
---
+++ lexi/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  environment: "prod",
+  firebase: {
+    apiKey: 'AIzaSyCsUpHmK5-o4hp8_HldvlaLU2gLOUVeHgY',
+    authDomain: 'lexis-ia.firebaseapp.com',
+    projectId: 'lexis-ia',
+    storageBucket: 'lexis-ia.firebasestorage.app',
+    messagingSenderId: '78825247320',
+    appId: '1:78825247320:web:2a69ba8ceabad513f3f02d',
+  },
+};
