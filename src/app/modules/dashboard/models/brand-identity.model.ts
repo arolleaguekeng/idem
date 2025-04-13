51c2dfb2a7e3942d0cd1f0544164d1780fa1f8c6
arolleaguekeng <arolleaguekeng@gmail.com>
2025-04-13 04:06:24 +0100

    create logo model

---
+++ src/app/modules/dashboard/models/brand-identity.model.ts
@@ -0,0 +1,9 @@
+export interface BrandIdentityModel {
+  brandDefinition: { content: string; summary: string };
+  toneOfVoice: { content: string; summary: string };
+  visualIdentityGuidelines: { content: string; summary: string };
+  typographySystem: { content: string; summary: string };
+  colorSystem: { content: string; summary: string };
+  iconographyAndImagery: { content: string; summary: string };
+  layoutAndComposition: { content: string; summary: string };
+}
