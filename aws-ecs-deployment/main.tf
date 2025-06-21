8f875d27b4c3773acca96d17aadfe8cc1bbf2fe4
romuald DJETEJE <romuald.djeteje@rhopenlabs.africa>
2025-06-21 22:45:08 +0100

    udapte file

--- aws-ecs-deployment/main.tf
+++ aws-ecs-deployment/main.tf
@@ -25,10 +25,10 @@ module "idem" {
   container_port            = var.idem
   task_cpu                  = 256
   task_memory               = 512
-  private_subnet_ids        = module.network.private_subnet_ids
-  public_subnet_ids         = module.network.public_subnet_ids
-  ecs_security_group_id     = module.security.ecs_sg_id
-  alb_security_group_id     = module.security.alb_sg_id
+  private_subnet_ids        = module.networking.private_subnet_ids
+  public_subnet_ids         = module.networking.public_subnet_ids
+  ecs_security_group_id     = module.networking.ecs_security_group_id
+  alb_security_group_id     = module.networking.alb_security_group_id
   ecs_task_execution_role_arn = module.iam.ecs_task_execution_role_arn
   vpc_id                    = module.network.vpc_id
   tags                      = var.tags
@@ -93,10 +93,10 @@ module "idem-webgen" {
   public_subnet_ids          = module.networking.public_subnet_ids
   ecs_security_group_id      = module.security.ecs_sg_id
   alb_security_group_id      = module.security.alb_sg_id
-  ecs_task_execution_role_arn = module.iam.task_execution_role_arn
+  ecs_task_execution_role_arn = module.iam.ecs_task_execution_role_arn
 
   vite_log_level             = var.vite_log_level
-  vite_api_base_url          = var.vite_api_base_url
+  vite_api_base_url          = module.idem-api.api_base_url
   gemini_api_key             = var.gemini_api_key
 
   vpc_id                     = module.networking.vpc_id
